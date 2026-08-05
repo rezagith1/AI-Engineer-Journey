efcba9a78d62a0b855c7f1696893a239f9eee1c7
Reza Vakili <reza.it95@gmail.com>
2026-08-05 23:36:44 +0330

    Complete Chapter 04 For Loop and update AI Engineer Book

---
+++ 01-Python/day09_for_loop.py
@@ -0,0 +1,15 @@
+
+for i in range(5):
+    print(i)
+
+for i in range(2,7):
+    print(i)
+
+for i in range(2,11,2):
+    print(i)
+
+for i in range(10,0,-2):
+    print(i)
+
+for i in range(1,6):
+    print("*"*i)    
